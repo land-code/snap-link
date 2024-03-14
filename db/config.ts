6ca8f797c1833482707ba126be87e336bf6dd134
David García Díaz <davidgrcdiaz@gmail.com>
2024-03-14 15:12:37 +0100

    Add create link page

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,15 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable } from 'astro:db';
+
+const Links = defineTable({
+  columns: {
+    title: column.text({ unique: true, optional: true }),
+    url: column.text()
+  }
+})
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: {
+    Links
+  }
 });
