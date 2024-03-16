1dbd353aac8bab6c9df29d342090b27a01870669
David García Díaz <davidgrcdiaz@gmail.com>
2024-03-16 13:30:14 +0100

    Add userId field to links

--- db/seed.ts
+++ db/seed.ts
@@ -1,8 +1,13 @@
-import { db, Links } from 'astro:db';
+import { db, Links, Users } from 'astro:db';
 
 export default async function() {
+  await db.insert(Users).values([
+    {
+      email: 'unrandom@gmail.com'
+    }
+  ])
   await db.insert(Links).values([
-    { title: 'Example', url: 'https://example.com' },
-    { title: 'Public example', url: 'https://public-example.com', public: true }
+    { title: 'Example', url: 'https://example.com', userId: 1  },
+    { title: 'Public example', url: 'https://public-example.com', public: true, userId: 1 }
   ])
 }
