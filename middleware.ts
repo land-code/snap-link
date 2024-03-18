d8c10701e4b2757b0f96951722c8f72557f0812a
David García Díaz <davidgrcdiaz@gmail.com>
2024-03-18 22:33:39 +0100

    Add edgeMiddleware option to Vercel adapter

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { Links, db, eq } from "astro:db"
+
+export const config = {
+  matcher: '/l/:path*'
+}
+
+export default async function middleware (request: Request) {
+  const url = new URL(request.url)
+
+  if (url.pathname.startsWith('/l/')) {
+    const title = url.pathname.slice(3)
+    const [{url: destination}] = await db.select().from(Links).where(eq(Links.title, title))
+    if (destination) {
+      return Response.redirect(destination, 301)
+    }
+  }
+}
