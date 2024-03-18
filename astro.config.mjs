a44059da1780c0fb6610c9e9a4cc9c9c2758a010
David García Díaz <davidgrcdiaz@gmail.com>
2024-03-18 22:12:00 +0100

    Add astro sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import db from "@astrojs/db";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db(), tailwind()],
+  integrations: [db(), tailwind(), sitemap()],
   output: "server",
   adapter: vercel()
 });
